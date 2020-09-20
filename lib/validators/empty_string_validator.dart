9bf6cd61a7b51abcdcca06019fb5f600996df20c
Denis Beketsky <tatsu.ukraine@gmail.com>
2020-09-20 23:42:49 +0300

    split empty validator on multiple

---
+++ lib/validators/empty_string_validator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+import 'field_validator.dart';
+
+/// Validates if [String] is empty
+class EmptyStringValidator extends FieldValidator<String> {
+
+  const EmptyStringValidator({
+    String fieldName,
+    @required String message
+  })  : super(
+    fieldName: fieldName,
+    message: message ?? 'should be empty',
+  );
+
+  const EmptyStringValidator.withMessage(String message)
+      : super.withMessage(message: message);
+
+  @override
+  bool isValid(String value) => value.isEmpty;
+}
